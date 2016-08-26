66da835af6c112a716b2765fc5b55cdec3e3e5b8
François Bonzon <francois@bikewm.com>
2016-08-26 02:08:40 +0200

    Make altitude label more prominent, add a pink theme

---
+++ compile_less.sh
@@ -0,0 +1,19 @@
+# Compiles .less files to CSS.
+
+set -e
+set -x
+
+pushd src/css/themes > /dev/null
+
+for theme in lime pink purple steelblue; do
+  lessc --verbose --clean-css ${theme}.less ${theme}.css
+  echo >> ${theme}.css
+done
+
+cd ..
+cat themes/*.css > L.Control.Elevation.css
+echo >> L.Control.Elevation.css
+
+popd > /dev/null
+
+echo "Done."
